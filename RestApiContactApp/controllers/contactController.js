af3f9374bdfae1415e91bd760ad5bc04db6ec431
JayTheCoder77 <jayantiyer90@gmail.com>
2025-04-30 16:57:03 +0530

    30 april 2025

--- RestApiContactApp/controllers/contactController.js
+++ RestApiContactApp/controllers/contactController.js
@@ -1,40 +1,45 @@
+import asyncHandler from "express-async-handler";
 // @desc Get All contacts
 // @route Get /api/contacts
 // @access public
 
-export const getContacts = (req,res) => {
+export const getContacts = asyncHandler(async(req,res) => {
     res.status(200).json({message : "get all contacts"});
-}
+})
 
 // @desc create contacts
 // @route Post /api/contacts
 // @access public
 
-export const createContact = (req,res) => {
-    console.log(`request body : ${req.body}`);
+export const createContact = asyncHandler(async(req,res) => {
+    console.log(`request body : ${JSON.stringify(req.body)}`);
+    const {name ,email , phone} = req.body
+    if(!name || !email || !phone){
+        res.status(400)
+        throw new Error("All fields are required")
+    }
     res.status(201).json({message : "create contacts"});
-}
+})
 
 // @desc get individual contacts
 // @route get /api/contacts/:id
 // @access public
 
-export const getContact = (req,res) => {
+export const getContact = asyncHandler(async(req,res) => {
     res.status(200).json({message : `get one contact : ${req.params.id}`});
-}
+})
 
 // @desc update individual contacts
 // @route put /api/contacts/:id
 // @access public
 
-export const updateContact = (req,res) => {
+export const updateContact = asyncHandler(async(req,res) => {
     res.status(200).json({message : `update contacts : ${req.params.id}`});
-}
+})
 
 // @desc delete individual contacts
 // @route delete /api/contacts/:id
 // @access public
-
-export const deleteContact = (req,res) => {
+export const deleteContact = asyncHandler(async(req,res) => {
     res.status(200).json({message : `delete contacts : ${req.params.id}`});
-}
+})
--- RestApiContactApp/server.js
+++ RestApiContactApp/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { config as configDotenv } from "dotenv";
 import contactRoute from "./routes/contactRoutes.js";
+import { errorHandler } from "./middleware/errorHandler.js";
 
 // Load environment variables from .env file
 configDotenv();
@@ -8,7 +9,9 @@ configDotenv();
 const app = express()
 const port = process.env.PORT || 5000
 
+app.use(express.json())
 app.use("/api/contacts" , contactRoute)
+app.use(errorHandler)
 
 app.listen(port, () => {
     console.log(`server is running on port ${port}`);
